80570ae1cf847a215b14ff3eaa621a9e00d6eaad
Beau Coburn <56836072+beaucoburn@users.noreply.github.com>
2025-01-30 06:50:46 -0600

    Update ConcatOfArray.js

--- ArraysAndHashing/ConcatOfArray.js
+++ ArraysAndHashing/ConcatOfArray.js
@@ -6,3 +6,13 @@ var getConcatenation = function(nums) {
     let ans = nums.concat(nums)
     return ans
 };
+
+var getConcatenation(nums) {
+    let ans = [];
+    for (let i = 0; i < 2; i++) {
+        for (let num of nums) {
+            ans.push(num);
+        }
+    }
+    return ans;
+}
